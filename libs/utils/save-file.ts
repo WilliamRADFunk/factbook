c062319d7da88f366440fb73a32c5585ab0e8bd6
William RAD Funk <contact@WILLIAMRADFunk.com>
2019-03-14 15:24:05 -0700

    Implements an auto-formatter for the outputted json files.

--- libs/utils/save-file.ts
+++ libs/utils/save-file.ts
@@ -1,9 +1,10 @@
 import * as fs from 'graceful-fs';
+import * as fj from 'formatter-json';
 
 import { store } from '../constants/globalStore';
 
 export function saveFile(fileName: string, storeName: string) {
-    let file = JSON.stringify(store[storeName]);
+    let file = fj(JSON.stringify(store[storeName]));
 	file = file.replace(/\\n/g, ' ');
 	fs.writeFileSync(`dist/${fileName}.json`, file);
 };
